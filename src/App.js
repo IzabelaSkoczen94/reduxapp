23670970e9b5c421ed1f237429ba87c6b7297831
IzabelaSkoczen94 <izabela.s94@gmail.com>
2019-06-03 13:45:52 +0200

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { createStore, combineReducers } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialMovies = {
+  listName: 'Favourite',
+  list: [
+    'Rambo III', 'Hakerzy', 'Matrix'
+  ]
 }
 
+const initialActors = {
+  listName: 'Best',
+  list: [
+    'Tom Hanks', 'Julia Roberts', 'Angelina Jolie'
+  ]
+}
+
+const initialStories = {
+  listName: 'The Best',
+  list: [
+    'Kubus', 'Toys', 'Gdzie jest Nemo'
+  ]
+}
+
+function movies(state = initialMovies, action) {
+  switch (action.type) {
+    case 'ADD_MOVIES':
+      return {
+        ...state, list: [...state.list, action.item]
+      }
+    case 'RESET_MOVIES':
+      return {
+        ...state, list: []
+      }
+    default:
+      return state
+  }
+}
+
+function actors(state = initialActors, action)
+{
+  switch (action.type) {
+    case 'ADD_ACTORS':
+      return {
+        ...state, list: [...state.list, action.item]
+      }
+    case 'RESET_ACTORS':
+      return {
+        ...state, list: []
+      }
+    default:
+      return state
+  }
+}
+
+function stories (state = initialStories, action)
+{
+  switch (action.type) {
+    case 'ADD_STORIES':
+    return {
+      ...state, list: [...state.list, action.item]
+    }
+    case 'RESET_ACTORS':
+      return {
+        ...state, list: []
+      }
+    default:
+      return state
+    
+  }
+}
+
+const reducer = combineReducers({movies, actors, stories })
+
+const store = createStore(reducer, composeWithDevTools())
+window.store = store
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Testowanie Redux
+      </div>
+    );
+  }
+}
 export default App;
