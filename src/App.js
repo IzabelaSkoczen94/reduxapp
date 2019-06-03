2598026f7e8222a2ac84ad300886e7210eb03fe2
IzabelaSkoczen94 <izabela.s94@gmail.com>
2019-06-03 15:49:47 +0200

    :App.js update

--- src/App.js
+++ src/App.js
@@ -1,86 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
-import { createStore, combineReducers, bindActionCreators } from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import React, { Component } from 'react'
+import './App.css'
+import { createStore } from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from './reducers'
 
-const initialMovies = {
-  listName: 'Favourite',
-  list: [
-    'Rambo III', 'Hakerzy', 'Matrix'
-  ]
-}
-
-const initialActors = {
-  listName: 'Best',
-  list: [
-    'Tom Hanks', 'Julia Roberts', 'Angelina Jolie'
-  ]
-}
-
-const initialStories = {
-  listName: 'The Best',
-  list: [
-    'Kubus', 'Toys', 'Gdzie jest Nemo'
-  ]
-}
-
-function movies(state = initialMovies, action) {
-  switch (action.type) {
-    case 'ADD_MOVIES':
-      return {
-        ...state, list: [...state.list, action.item]
-      }
-    case 'RESET_MOVIES':
-      return {
-        ...state, list: []
-      }
-    default:
-      return state
-  }
-}
-
-function actors(state = initialActors, action)
-{
-  switch (action.type) {
-    case 'ADD_ACTORS':
-      return {
-        ...state, list: [...state.list, action.item]
-      }
-    case 'RESET_ACTORS':
-      return {
-        ...state, list: []
-      }
-    default:
-      return state
-  }
-}
-
-function stories (state = initialStories, action)
-{
-  switch (action.type) {
-    case 'ADD_STORIES':
-    return {
-      ...state, list: [...state.list, action.item]
-    }
-    case 'RESET_MOVIES':
-      return {
-        ...state, list: []
-      }
-    default:
-      return state
-    
-  }
-}
-
-const reducer = combineReducers({movies, actors, stories })
-const store = createStore(reducer, composeWithDevTools())
-
-store.dispatch({type: 'ADD_ACTORS', item: 'Boguslaw Linda'})
-const addActor = item => ({type: 'ADD_ACTORS', item})
-store.dispatch(addActor('Borys Szyc'))
-
-const creators = bindActionCreators({addActor}, store.dispatch)
-creators.addActor('Jan Frycz')
+const store = createStore(rootReducer, composeWithDevTools())
+window.store = store
 
 class App extends Component {
   render() {
