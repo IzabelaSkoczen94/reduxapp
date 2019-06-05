6c7e307940bef00c2ef89eee4b9a648f42bca0b8
IzabelaSkoczen94 <izabela.s94@gmail.com>
2019-06-05 14:31:40 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
-import { createStore } from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import rootReducer from './reducers'
+import MoviesContainer from './app/movies/components/MoviesContainer'
 
-const store = createStore(rootReducer, composeWithDevTools())
-window.store = store
 
 class App extends Component {
   render() {
     return (
       <div> 
         Testowanie Redux
+        <MoviesContainer/>
       </div>
     );
   }
